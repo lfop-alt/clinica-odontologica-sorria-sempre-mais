21612406efd4f5b944c8541b30246378ae78d79d
lfped <lfpedro@outlook.com.br>
2023-04-26 11:49:59 -0300

    input backend basic

---
+++ Backend/src/main/java/com/br/ontonto/OntontoApplication.java
@@ -0,0 +1,13 @@
+package com.br.ontonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntontoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/br/ontonto/OntontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ontonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
